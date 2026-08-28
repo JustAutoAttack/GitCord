df9943c7535e9781e864ce91a83d6963075c8637
Joseph Moro <moroajoseph@gmail.com>
2026-08-28 07:00:21 -0400

    test

--- apps/bot/src/config/env.ts
+++ apps/bot/src/config/env.ts
@@ -9,16 +9,18 @@ export const ENV = {
 	PORT: Number(process.env.PORT) || 3000,
 	DISCORD_BOT_TOKEN: process.env.DISCORD_BOT_TOKEN || '',
 	DISCORD_CHANNEL_ID: process.env.DISCORD_CHANNEL_ID || '',
-	DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET || ''
+	DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET || '',
+	NGROK_AUTHTOKEN: process.env.NGROK_AUTHTOKEN || ''
 };
 
 if (
 	!ENV.DISCORD_BOT_TOKEN ||
 	!ENV.DISCORD_CHANNEL_ID ||
-	!ENV.DISCORD_CLIENT_SECRET
+	!ENV.DISCORD_CLIENT_SECRET ||
+	!ENV.NGROK_AUTHTOKEN
 ) {
 	console.error(
-		'[Config] Missing required environment variables: DISCORD_BOT_TOKEN, DISCORD_CHANNEL_ID, or DISCORD_CLIENT_SECRET'
+		'[Config] Missing required environment variables: DISCORD_BOT_TOKEN, DISCORD_CHANNEL_ID, DISCORD_CLIENT_SECRET, or NGROK_AUTHTOKEN'
 	);
 	process.exit(1);
 }
